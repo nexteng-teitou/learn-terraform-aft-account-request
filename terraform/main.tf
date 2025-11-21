8cf0edc6090e8795fe407fa5ed6436e1412ee8cc
nexteng-teitou <teitou@nexteng.net>
2025-11-21 13:53:09 +0900

    create test account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,27 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "chengtao+new1@win-techno.com"
+    AccountName               = "aft new test 1"
+    ManagedOrganizationalUnit = "sandbox (ou-bkdg-vjosxunb)"
+    SSOUserEmail              = "chengtao+aftmanagement@win-techno.com"
+    SSOUserFirstName          = "aft"
+    SSOUserLastName           = "new test 1"
   }
 
   account_tags = {
     "Learn Tutorial" = "AFT"
+    "TestTag1" = "TestValue1"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "teitou"
+    change_reason       = "add test new 1 account"
   }
 
   custom_fields = {
     group = "non-prod"
+    testmeta2 = "testmetavalue2"
   }
 
   account_customizations_name = "sandbox"
